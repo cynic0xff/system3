fb8fb829fcfa2ba09879c8b56fe686e63e90fb7b
Scott Wren <cynics0xff@gmail.com>
2019-08-23 16:10:22 -0400

    Add node-watch to watch a folder and process the correct .csv file

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const System3 = require('./class/system3.js');
+const watch = require('node-watch');
 
 //settings
 const srcFilename = './data.csv';
+const pathToWatch = 'data';
 
-const sys3 = new System3(srcFilename);
-sys3.run();
+//watch the directory and run system3 when a file has been uploaded
+watch(pathToWatch, { recursive: false, filter: /\.csv$/ }, (evt, name) => {
+
+  //what event are we going to process
+  switch(evt) {
+    case 'update':
+      console.log('Processing %s', name, evt);
+
+      //create an instance of system3 and run
+      const sys3 = new System3(srcFilename);
+      sys3.run();
+      break;
+    case 'remove':
+      console.log('Deleting %s', name, evt);
+      break;
+    default:
+        console.log('Defaultg %s', name, evt);
+        break;
+  }
+
+
+
+});
