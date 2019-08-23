b67b1339d2f8a0b31ff751fd6a548143233f70af
Scott Wren <cynics0xff@gmail.com>
2019-08-23 15:47:24 -0400

    Add destination header information

--- class/system3.js
+++ class/system3.js
@@ -4,16 +4,26 @@ const Config = require('./config.js')
 
 class System3 {
     constructor(srcFile) {
+        //source file
         this.srcFile = srcFile;
 
-        console.log(this.srcFile);
+        //destination database headers
+        this.destHeaderArray = [
+            "DealerID","StockNumber","VIN","Year","Make","Model","TrimLevel","Condition","ExteriorColor","InteriorColor","EngineDescription","Transmission","DriveTrain",
+            "NumberofDoors","vehicleType","InventorySince","AgeInDays","InServiceDate","Mileage","StickerPrice","InternetPrice","InvoicePrice","MSRPPrice","ThirdPartyPrice",
+            "InternetSpecial","DealerCertified","OEMCertified","CertificationNumber","LotLocation","Balance","VehicleDescription","ThirdPartyDescription","ShowroomTitle",
+            "Memo","PictureURLs","Options","OptionCodes","InStock","VideoLink","3rdPartyVideoLink","CityMPG","HighwayMPG","RVType","Length","WaterCapacity","SlideOuts",
+            "SleepingCapacity","FuelType","AirConditioners","Awnings","LevelingJacks","DryWeight","TrailerWeight","Chassis","EngineManufacturer","EngineModel"
+        ];
+
+        //the array of vehicle information
+        this.data = [];
     }
 
     run() {
         fs.createReadStream(this.srcFile)
         .pipe(csv())
         .on('data', (row) => {
-            //console.log(row);
             this.process(row);
         })
         .on('end', () => {
@@ -22,7 +32,7 @@ class System3 {
     }
 
     process(row) {
-        console.log(row['first_name']);
+        console.log(this.destHeaderArray);
     }
 };
 
